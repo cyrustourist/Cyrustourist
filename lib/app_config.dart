756256de39b7b41d3810e37c866d19119ffb37e9
cyrustourist <tirandazkourosh62@gmail.com>
2026-08-22 17:40:46 +0330

    Create app_config.dart

---
+++ lib/app_config.dart
@@ -0,0 +1,15 @@
+class AppConfig {
+  static const String appName = 'Cyrus Tourist';
+
+  static const String appVersion = '1.0.0';
+
+  static const String defaultLanguage = 'fa';
+
+  static const String defaultCountry = 'Iran';
+
+  static const double defaultMapZoom = 12.0;
+
+  static const double userLocationZoom = 16.0;
+
+  static const String supportEmail = 'support@cyrustourist.ir';
+}
