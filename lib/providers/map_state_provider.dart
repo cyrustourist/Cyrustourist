1299102ee5662c316fb33fdf25b6c1a4c180375f
cyrustourist <tirandazkourosh62@gmail.com>
2026-08-22 18:00:09 +0330

    Update map_state_provider.dart

--- lib/providers/map_state_provider.dart
+++ lib/providers/map_state_provider.dart
@@ -1,1 +1,32 @@
 
+import 'package:flutter/foundation.dart';
+import '../services/map_state_service.dart';
+
+class MapStateProvider extends ChangeNotifier {
+  final MapStateService _service = MapStateService();
+
+  bool get isLoading => _service.isLoading;
+  bool get hasLocation => _service.hasLocation;
+  bool get isGpsEnabled => _service.isGpsEnabled;
+  String get message => _service.message;
+
+  Future<void> initialize() async {
+    await _service.initialize();
+    notifyListeners();
+  }
+
+  Future<void> refreshLocation() async {
+    await _service.refreshLocation();
+    notifyListeners();
+  }
+
+  void setLoading(bool value) {
+    _service.setLoading(value);
+    notifyListeners();
+  }
+
+  void updateMessage(String value) {
+    _service.updateMessage(value);
+    notifyListeners();
+  }
+}
