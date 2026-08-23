9a157dcb6e17027a03804b3ba9d15b532b8c3a4a
cyrustourist <tirandazkourosh62@gmail.com>
2026-08-23 03:49:40 +0330

    Implement LanguageManager for app language handling
    Added LanguageManager class to manage app languages and save preferences.

--- lib/core/language/app_language.dart
+++ lib/core/language/app_language.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+enum AppLanguage {
+  persian,
+  english,
+  arabic,
+}
+
+class LanguageManager {
+  static AppLanguage current = AppLanguage.english;
+
+  static Future<void> load() async {
+    final pref = await SharedPreferences.getInstance();
+
+    final saved = pref.getString('language');
+
+    if (saved == 'fa') {
+      current = AppLanguage.persian;
+    } else if (saved == 'ar') {
+      current = AppLanguage.arabic;
+    } else if (saved == 'en') {
+      current = AppLanguage.english;
+    } else {
+      final code = WidgetsBinding
+          .instance
+          .platformDispatcher
+          .locale
+          .languageCode
+          .toLowerCase();
+
+      if (code == 'fa') {
+        current = AppLanguage.persian;
+      } else if (code == 'ar') {
+        current = AppLanguage.arabic;
+      } else {
+        current = AppLanguage.english;
+      }
+    }
+  }
+
+  static Future<void> setLanguage(
+    AppLanguage language,
+  ) async {
+    current = language;
+
+    final pref = await SharedPreferences.getInstance();
+
+    switch (language) {
+      case AppLanguage.persian:
+        await pref.setString('language', 'fa');
+        break;
+
+      case AppLanguage.english:
+        await pref.setString('language', 'en');
+        break;
+
+      case AppLanguage.arabic:
+        await pref.setString('language', 'ar');
+        break;
+    }
+  }
+}
