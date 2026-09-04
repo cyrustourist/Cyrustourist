e0bfeafc24adf2bda8abba80dd3caadeeeddcec8
Cyrustourist <tirandazkourosh62@gmail.com>
2026-09-04 12:43:22 +0330

    Set apiKey for CartoConfig
    Updated apiKey to include the correct URL for basemaps.

--- lib/config/carto_config.dart
+++ lib/config/carto_config.dart
@@ -1,5 +1,5 @@
 class CartoConfig {
-  static const String apiKey = '';
+  static const String apiKey = 'https://basemaps.cartocdn.com/rastertiles/voyager/%7Bz%7D/%7Bx%7D/%7By%7D.png?key=YOUR_KEY
 
   static const String tileUrl =
       'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
