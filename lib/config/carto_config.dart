6d66796f0348f46dcd4d91d53b04728e620c43f7
Cyrustourist <tirandazkourosh62@gmail.com>
2026-09-04 12:40:08 +0330

    Add CartoConfig class with API configuration

---
+++ lib/config/carto_config.dart
@@ -0,0 +1,8 @@
+class CartoConfig {
+  static const String apiKey = '';
+
+  static const String tileUrl =
+      'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
+
+  static const String providerName = 'CARTO';
+}
