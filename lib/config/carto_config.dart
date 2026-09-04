cb9b3aa1dd9d02d3171f07cfea1cd50a8211ee34
Cyrustourist <tirandazkourosh62@gmail.com>
2026-09-04 13:03:02 +0330

    Update carto_config.dart

--- lib/config/carto_config.dart
+++ lib/config/carto_config.dart
@@ -1,8 +1,12 @@
 class CartoConfig {
-  static const String apiKey = 'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=YOUR_KEY
+  // کلید واقعی CARTO را اینجا قرار بده
+  static const String apiKey = 'YOUR_CARTO_API_KEY';
 
   static const String tileUrl =
       'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
+
+  static String get tileUrlWithKey =>
+      '$tileUrl?key=$apiKey';
 
   static const String providerName = 'CARTO';
 }
